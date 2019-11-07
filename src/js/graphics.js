b90d71e2a5c9cfa3fa2f02de24ca9bce43e0e3d0
Edwuards <48424617+Edwuards@users.noreply.github.com>
2019-11-06 20:42:37 -0600

    graphics seem to be complete , will work on testing them

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -101,7 +101,7 @@ function Graphic (data) {
   data.points = new Points(data.points);
   Plane.call(this,data.points);
   Context.call(this,data.canvas);
-  // create METHODS and properties
+ 
   
   
 }
@@ -201,105 +201,75 @@ function Polygon (data) {
 }
 
 function Rectangle(data){
-  if(typeof data !== 'object' || typeof data.width !== 'number' || typeof data.height !== 'number' || typeof data.x !== 'number' || typeof data.y !== 'number' ){
-    throw 'The data must have the following structure --> {x: int, y: int, width: int, height: int }'
+  
+  let test = undefined;
+  [
+    Rules.is.object(data),
+    Rules.has.properties(['x','y','w','h']),
+    (()=>{
+      ['x','y','w','h'].every((prop)=>{
+        test = Rules.is.number(data[prop]);
+        return test.passed;
+      });
+      return test
+    })
+  ].some((check)=>{ test = check; return !test.passed });
+  
+  if(!test.passed){ throw test.error(); }
+  
+  {
+    let x = data.x, w = x+data.w;
+    let y = data.y, h = y+data.h;
+    data.points = [[x,y],[w,y],[w,h],[x,h]];
+    Polygon.call(this,data);
   }
-  Object.assign(this,new Polygon({points: [
-    {x:data.x, y:data.y},
-    {x:data.x + data.width, y:data.y},
-    {x:data.x + data.width, y:data.y + data.height},
-    {x:data.x, y:data.y + data.height}], context: data.context || {} }));
   
 }
 
+
 function Square (data) {
-  if (typeof data !== 'object' || (typeof data.x !== 'number' && typeof data.y !== 'number' && typeof data.size !== 'number')) {
-    throw 'The data must have the following structure --> {x: int, y: int size: int}'
+  
+  let test = undefined;
+  [
+    Rules.is.object(data),
+    Rules.has.properties(['x','y','size']),
+    (()=>{
+      ['x','y','size'].every((prop)=>{
+        test = Rules.is.number(data[prop]);
+        return test.passed;
+      });
+      return test
+    })
+  ].some((check)=>{ test = check; return !test.passed });
+  
+  if(!test.passed){ throw test.error(); }
+  
+  {
+    let x = data.x, w = x+data.size;
+    let y = data.y, h = y+data.size;
+    data.points = [[x,y],[w,y],[w,h],[x,h]];
+    Polygon.call(this,data);
   }
-  let square = {
-    instance: this,
-    size: data.size
-  }
-  Object.assign(this, new Polygon({ points:
-    [{ x: data.x, y: data.y }, { x: data.x + data.size, y: data.y }, { x: data.x + data.size, y: data.y + data.size }, { x: data.x, y: data.y + data.size }]
-  }))
-  this.get.size = () => { return square.size }
-  this.set.size = (size,origin) => {
-    if (typeof size !== 'number' || size < 0 ) { throw 'The paramter must be a number and greater than 0 ' }
-    if(origin === undefined){ origin = square.instance.get.points(0).position; }
-    square.instance.transform.scale((size / square.size),origin)
-    square.size = size
-  }
-  this.actions.define({
-    name: 'size',
-    validate: (data) => {
-        let response = {error: false, message: ''}
-        if (typeof data !== 'object' || typeof data.size !== 'number' || data.size < 0 ) { response.error = true; response.message = 'The paramter must be a number and greater than 0 ' }
-        return response
-      },
-      action: function (data) {
-        let size = this.graphic.get.size();
-        if (this.progress === this.duration) {
-          data.step = (data.size - size)/this.duration
-          if (data.origin == undefined) { data.origin = this.graphic.get.points(0).position }
-        }
-        this.graphic.set.size(size+data.step,(typeof data.origin === 'function' ? data.origin() : data.origin))
-      }
-  })
+  
+  
 }
 
 function Circle (data){
-  if(typeof data !== 'object' || typeof data.x !== 'number' || typeof data.y !== 'number' || typeof data.radius !== 'number'){
-    throw 'The data must have the following structure --> {x: int, y:int ,radius: int}'
-  }
+  data.angle = { start: 0, finish: 360};
+  Arc.call(this,data);
   
-  Object.assign(this,new Arc({x:data.x,y:data.y,radius: data.radius, angle: {start: 0, finish: 360}, context: data.context || {} }))
-  
-  let circle = {
-    instance: this,
-    circumfrence: data.radius * 2
-  }
-  
-  setOfBasicActions(this)
-  
-  this.get.circumfrence = () => { return circle.circumfrence }
-  this.set.circumfrence = (int) => {
-    if (typeof int !== 'number' || int < 1) { throw 'The parameter must be a number and greater than 0' }
-    circle.circumfrence = int
-    circle.instance.set.radius(int/2)
-  }
-  this.transform.scale = (size,origin) => {
-    circle.instance.set.radius(circle.instance.get.radius() * size)
-    circle.instance.get.points().forEach((pt) => {
-      pt.x -= origin.x
-      pt.y -= origin.y
-      pt.x *= size
-      pt.y *= size
-      pt.x += origin.x
-      pt.y += origin.y
-    })
-  }
-}
-
-const available = {
-  'polygon': Polygon,
-  'square': Square,
-  'rectangle': Rectangle,
-  'circle': Circle
-}
-
-function Graphics (Layers) {
-  let expose = {};
-  for(let graphic in available){
-    expose[graphic] = (create) => {
-      if(typeof create !== 'object' || typeof create.data !== 'object' || typeof create.layer !== 'number'){
-        throw 'The data must have the following structure --> { data: object , layer: number }'
-      }
-      return Layers.get(create.layer).graphics.add(new available[graphic](create.data) )
+  const METHODS = {
+    'circumference': {
+      enumerable: true,
+      get: function(){ return (this.radius * 2) }
     }
   }
-  this.create = expose
   
+  Object.defineProperties(this,METHODS);
+  
+ 
 }
 
-export { Polygon, Graphic }
+
+
+export { Polygon, Rectangle, Square, Circle, Arc }

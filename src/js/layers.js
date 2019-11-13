8d603d46dcccdfb6aacd8e8fbb3e97f71b39eafa
Cesar Edwuards <48424617+Edwuards@users.noreply.github.com>
2019-11-12 21:07:56 -0800

    I am currently working on the Layers constructor function. We need to create the remove and find functions. We then must create an Observable Object. This Layers function must expose a register and unregister method that will allow other object to register to an event emitted by this object. We are also elaborting how the render function and graphics will manipulate eachother. Don't forget to add a remove function for the Points object inside the geometry module.

--- src/js/layers.js
+++ src/js/layers.js
@@ -11,49 +11,35 @@ function Layers (container) {
     'add': {
       enumerable: true,
       writable: false,
-      value: ()=>{
+      value: (layer)=>{
+        layer.index = LAYERS.length;
+        LAYERS.push(new Layer(layer));
+        return LAYERS[layers.index];
+      }
+    },
+    'get':{
+      enumerable: true,
+      get:(index)=>{
+        if(index !== undefined){
+          let test = Test([ [Rules.is.number,[index]],[Rules.has.index,[LAYERS,index]] ]);
+          if(!test.passed){ throw test.error; }
+          return LAYERS[index];
+        }
+
+        return LAYERS.map((l)=>{ return l });
 
       }
     }
   }
 
-  this.add = (name, width, height) => {
-    let layer = {
-      id: Layers.length,
-      index: Layers.length,
-      name: name || 'Untitled-' + Layers.length,
-      height: height || container.offsetHeight,
-      width: width || container.offsetWidth
-    }
-    layer = new Layer(layer)
-    container.appendChild(layer.get.html())
-    return Layers[Layers.push(layer) - 1]
-  }
-  this.get = (index) => {
-    if (index !== undefined) {
-      if (index !== undefined && typeof index !== 'number') { throw 'The index paramter must be a number' } else if (Layers[index] === undefined) { throw 'The index you supplied does is not defined in your points array' };
-      return Layers[index]
-    }
-    let copy = []
-    Layers.forEach((layer) => { copy.push(layer) })
-    return copy
-  }
-  this.find = (key) => {
-    if (typeof key !== 'object' || key.value === undefined || key.name === undefined || ['name', 'index', 'id'].every((name) => { return key.name !== name })) {
-      throw 'The key paramter must a valid key object --> {name : "index" || "id" || "name" , value: "int" || "string" } '
-    } else if ((key.name === 'index' || (key.name === 'id' && typeof key.value !== 'number')) || (key.name === 'name' && typeof key.value !== 'string')) {
-      throw 'The key paramter must a valid key object --> {name : "index" || "id" || "name" , value: "int" || "string" '
-    }
-    let result = Layers.find((layer) => { return layer.get[key.name]() === key.value })
-    if (result === undefined) { throw 'There was no match for the layer with the "' + key.name + '" property equivalent to the value "' + key.value + '"' };
-    return result
-  }
+  
 }
 
 function Layer (data) {
   let test = Test([
     [Rules.is.object,[data]],
     [Rules.has.properties,[['name','index','width','height'],data]],
+    [Rules.has.arrayLength,[Object.keys(data),4]],
     [Rules.is.string,[data.name]],
     [(data)=>{
       let test = undefined;

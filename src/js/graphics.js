4c6af97a2cf5d8d5d8637792225a280a4c8f63be
Cesar Edwuards <48424617+Edwuards@users.noreply.github.com>
2020-02-04 22:07:24 -0800

    Created methods for the Context object of a graphic. This allows for each graphic to manipluate the canvas object its tied to in an independant form

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -6,89 +6,91 @@ import { Actions, setOfBasicActions } from './actions.js';
 
 const ID = Helpers.counter();
 
-function Context(canvas){
-  let test = Test([
-    [Rules.is.object,[canvas]],
-    [Rules.is.instanceOf,[canvas,CanvasRenderingContext2D]]
-  ]);
-  if(!test.passed){ throw test.error; }
 
-  const GRAPHIC = this;
-  const CANVAS = canvas;
-  const CONTEXT = { properties: {}, functions: {} };
-  const CALL = ()=>{
-    for (let key in CONTEXT.functions) {
-      if(CONTEXT.functions[key].state){ CANVAS[key].apply(CANVAS,CONTEXT.functions[key].args)}
-    }
-  }
-  const SETUP = ()=>{
-    for (let prop in CONTEXT.properties) { CANVAS[prop] = CONTEXT.properties[prop]; }
-  }
-  for(let key in CANVAS){
-    if(typeof CANVAS[key] !== 'function' && key !== 'canvas'){ CONTEXT.properties[key] = CANVAS[key]; }
-    else{ CONTEXT.functions[key] = {state: false, args: [] }; }
-  }
-  CONTEXT.functions.fill.state = true;
-  const METHODS = {
-    'render': {
-      configurable: true,
-      enumerable: true,
-      set: (render)=>{
-        let test = Rules.is.function(render)
-        if(!test.passed){ throw test.error; }
-        Object.defineProperty(GRAPHIC,'render',{
-          enumerable: true,
-          writable: false,
-          value: ()=>{
-            CANVAS.save();
-            CANVAS.beginPath();
-            SETUP();
-            render.call({graphic: GRAPHIC, canvas: CANVAS })
-            CALL();
-            CANVAS.closePath();
-            CANVAS.restore();
-          }
-        })
+  function Context(canvas){
+    let test = Test([
+      [Rules.is.object,[canvas]],
+      [Rules.is.instanceOf,[canvas,CanvasRenderingContext2D]]
+    ]);
+    if(!test.passed){ throw test.error; }
 
+    const GRAPHIC = this;
+    const CANVAS = canvas;
+    const CONTEXT = { properties: {}, functions: {} };
+    const CALL = ()=>{
+      for (let key in CONTEXT.functions) {
+        if(CONTEXT.functions[key].state){ CANVAS[key].apply(CANVAS,CONTEXT.functions[key].args);}
       }
-    },
-    'context':{
-      enumerable: true,
-      writable: false,
-      value: (()=>{
-        let obj = {};
-        for (let key in CANVAS) {
-          if(typeof CANVAS[key] !== 'function' ){
-            Object.defineProperty(obj,key,{
-              enumerable: true,
-              get: ()=>{ return CONTEXT.properties[key]; },
-              set: (value)=>{ CONTEXT.properties[key] = value; }
-            });
-          }
-          else{
-            Object.defineProperty(obj,key,{
-              enumerable: true,
-              writable: false,
-              value: function(){
-                if(arguments.length === 1 && typeof arguments[0] === 'boolean'){ CONTEXT.functions[key].state = arguments[0]; }
-                if(arguments.length >= 1 && typeof arguments[0] != 'boolean'){
-                  CONTEXT.functions[key].state = true;
-                  CONTEXT.functions[key].args = arguments;
-                }
-                return true;
-              }
-            });
-          }
-        }
-
-        return obj;
-      })()
+    };
+    const SETUP = ()=>{
+      for (let prop in CONTEXT.properties) { CANVAS[prop] = CONTEXT.properties[prop]; }
+    };
+    for(let key in CANVAS){
+      if(typeof CANVAS[key] !== 'function' && key !== 'canvas'){ CONTEXT.properties[key] = CANVAS[key]; }
+      else{ CONTEXT.functions[key] = {state: false, args: [] }; }
     }
+    CONTEXT.functions.fill.state = true;
+    const METHODS = {
+      'render': {
+        configurable: true,
+        enumerable: true,
+        set: (render)=>{
+          let test = Rules.is.function(render);
+          if(!test.passed){ throw test.error; }
+          Object.defineProperty(GRAPHIC,'render',{
+            enumerable: true,
+            writable: false,
+            value: ()=>{
+              CANVAS.save();
+              CANVAS.beginPath();
+              SETUP();
+              render.call({graphic: GRAPHIC, canvas: CANVAS });
+              CALL();
+              CANVAS.closePath();
+              CANVAS.restore();
+            }
+          });
+
+        }
+      },
+      'context':{
+        enumerable: true,
+        writable: false,
+        value: (()=>{
+          let obj = {};
+          for (let key in CANVAS) {
+            if(typeof CANVAS[key] !== 'function' ){
+              Object.defineProperty(obj,key,{
+                enumerable: true,
+                get: ()=>{ return CONTEXT.properties[key]; },
+                set: (value)=>{ CONTEXT.properties[key] = value; }
+              });
+            }
+            else{
+              Object.defineProperty(obj,key,{
+                enumerable: true,
+                writable: false,
+                value: function(){
+                  if(arguments.length === 1 && typeof arguments[0] === 'boolean'){ CONTEXT.functions[key].state = arguments[0]; }
+                  if(arguments.length >= 1 && typeof arguments[0] != 'boolean'){
+                    CONTEXT.functions[key].state = true;
+                    CONTEXT.functions[key].args = arguments;
+                  }
+                  return true;
+                }
+              });
+            }
+          }
+
+          return obj;
+        })()
+      }
+    };
+
+    Object.defineProperties(this,METHODS);
+
   }
 
-  Object.defineProperties(this,METHODS);
-
-}
 
 function Graphic (data) {
   let test = Test([
